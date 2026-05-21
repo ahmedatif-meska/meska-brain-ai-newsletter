f09e8f2b7a22dca0c07c5e5d996c91a19ca8f199
Ahmed Atif <ahmedatif@meska.ai>
2026-05-21 07:27:18 +0300

    push it

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,10 +42,16 @@ export default function RootLayout({
       className={`${jakarta.variable} ${inter.variable} ${carlito.variable} h-full w-full max-w-full overflow-x-clip antialiased`}
     >
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=block"
         />
       </head>
       <body className="flex min-h-[100dvh] w-full max-w-full flex-col overflow-x-clip">
